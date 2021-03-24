fcdc169114e9a4ca0fe7f636cd83edfa9918a9c1
Christian Ndu <christiannduh@gmail.com>
2021-03-24 15:49:57 +0100

    everything works, yaaay!

--- index.js
+++ index.js
@@ -1,82 +1,80 @@
 const express = require("express");
 const app = express();
-require("dotenv").config()
+require("dotenv").config();
 const fetch = require("node-fetch");
 
-const baseURL = "https://api.paystack.co/"
+const baseURL = "https://api.paystack.co";
+const exampleRef = "ejzk5zimdyzbsi3";
+let userAuthCode;
 
 const PORT = process.env.PORT || 5006;
 
 app.post("/charge-user", async (req, res) => {
-    
-    try {
-        const params = JSON.stringify({
-                email: "customer@email.com",
-                amount: "10000",
-                bank: {
-                code: "057",
-                account_number: "0000000000",
-            },
-                birthday: "1995-12-23",
-        });
+  try {
+    const params = JSON.stringify({
+      email: "customer@email.com",
+      amount: "10000",
+      bank: {
+        code: "057",
+        account_number: "0000000000",
+      },
+      birthday: "1995-12-23",
+    });
 
-        const fetchData = await fetch(`${baseURL}/charge`, {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${process.env.SECRET_KEY}`,
-            "Content-Type": "application/json",
-          },
-          body: params,
-        });
-        const response = fetchData.json();
-        res.json(response);
-    } catch (error) {
-        console.error(err)
-    }
-  
+    const fetchData = await fetch(`${baseURL}/charge`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.SECRET_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: params,
+    });
+    const response = await fetchData.json();
+    res.json(response);
+  } catch (err) {
+    console.error(err);
+  }
 });
 let transactionState = false;
 
 app.get("/verify-charge", async (req, res) => {
-    
   try {
     const fetchData = await fetch(
-      `${baseURL}/transaction/verify/:reference`,
+      `${baseURL}/transaction/verify/${exampleRef}`,
       {
         method: "GET",
         headers: {
           Authorization: `Bearer ${process.env.SECRET_KEY}`,
           "Content-Type": "application/json",
         },
-        body: params,
       }
     );
-    const response = fetchData.json();
+    const response = await fetchData.json();
     res.json(response);
-    if(response.data.status){
-        transactionState = true
-        const {authourization_code} = response.data.authourization
-        const {email} = response.data.customer
-        res.json({
-            auth_code : authourization_code,
-            email
-        })
+    if (response.data.status) {
+      transactionState = true;
+      const { authourization_code } = response.data.authourization; // empty since the charge has not been completed
+      userAuthCode = authourization_code;
+      const { email } = response.data.customer;
+      res.json({
+        auth_code: authourization_code,
+        email,
+      });
     }
-
-  } catch (error) {
+  } catch (err) {
     console.error(err);
   }
 });
 
-if(transactionState){
-    const params = JSON.stringify({
-  "authorization_code" : authorization_code,
-  "email" : "mail@mail.com",
-  "amount" : 300000
-})
-    app.post("/recurring-charge", (req,res) => {
-        try {
-    const fetchData = await fetch(`${baseURL}/transaction/charge_authorization`,
+app.post("/recurring-charge", async (req, res) => {
+  const params = JSON.stringify({
+    authorization_code: userAuthCode || "",
+    email: "mail@mail.com",
+    amount: 300000,
+  });
+  try {
+    const fetchData = await fetch(
+      `${baseURL}/transaction/charge_authorization`,
       {
         method: "POST",
         headers: {
@@ -86,15 +84,11 @@ if(transactionState){
         body: params,
       }
     );
-    const response = fetchData.json();
+    const response = await fetchData.json();
     res.json(response);
-        }
-    catch (error) {
+  } catch (err) {
     console.error(err);
   }
-})
-}
-
-
+});
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
